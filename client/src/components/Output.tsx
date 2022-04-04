dd23573052668dce2a227cfd7c1feae4d79f63ab
Ramón Lamana <rlamana@gmail.com>
2022-04-04 12:30:22 +0200

    Automatic scroll to bottom in output

--- client/src/components/Output.tsx
+++ client/src/components/Output.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useMemo } from 'react';
+import { useEffect, useRef, useMemo } from 'react';
 import { Box } from '@mui/material';
 import OutputLine from './OutputLine';
 
@@ -13,31 +13,40 @@ const convertOutput = (output: string): Array<string> => {
 };
 
 function Output({ output }: OutputProps) {
+  const scrollable = useRef<HTMLDivElement | null>(null);
   const lines = useMemo(() => {
     return convertOutput(output);
   }, [output]);
 
+  useEffect(() => {
+    scrollable?.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [lines]);
+
   return (
-    <Box component="div" sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      flex: 1,
-      width: '100%',
-      px: 3,
-      py: 2,
-      bgcolor: '#101010',
-      color: 'grey.300',
-      border: '0',
-      borderRadius: 1,
-      boxShadow: 1,
-      fontSize: '0.8rem',
-      fontWeight: 'normal',
-      overflowX: 'hidden',
-      overflowY: 'auto'
-    }}>
+    <Box
+      component="div"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        width: '100%',
+        px: 3,
+        py: 2,
+        bgcolor: '#101010',
+        color: 'grey.300',
+        border: '0',
+        borderRadius: 1,
+        boxShadow: 1,
+        fontSize: '0.8rem',
+        fontWeight: 'normal',
+        overflowX: 'hidden',
+        overflowY: 'auto'
+      }}
+    >
       {lines.map((line, i) => (
         <OutputLine key={`Line-${i}`} line={line} />
       ))}
+      <div ref={scrollable} />
     </Box>
   );
 }

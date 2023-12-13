ed2ed9e2eeadee222fa78b2708fb63e676d2dde7
Ramon Lamana <rlamana@gmail.com>
2023-12-13 17:57:55 +0100

    New layout for the main view when no context

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
 import logoDark from '../images/logo-dark.svg';
@@ -17,79 +17,203 @@ function Login() {
   const handleCloseDialog = () => {
     setOpen(false);
   };
-  
+
   const handleTrial = () => {
     window.ddClient.host.openExternal('https://www.okteto.com/free-trial/');
-  }
+  };
 
   return (
-    <Box sx={{
-      display: 'flex',
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-      gap: 2
-    }}>
-      <Box sx={{
+    <Box
+      sx={{
         display: 'flex',
-        flexDirection: 'row',
+        flex: 1,
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        gap: 6
-      }}>
-        <Box sx={{
+        height: '100%',
+        gap: 2,
+      }}
+    >
+      <Box
+        sx={{
           display: 'flex',
-          borderRight: '1px solid',
-          borderColor: 'white',
-          height: '100%',
+          flexDirection: 'row',
+          alignItems: 'center',
           justifyContent: 'center',
-          pr: 6,
-        }}>
-          <img src={theme.palette.mode === 'dark' ? logoDark : logoLight} width="240" />
-        </Box>
-        <Box sx={{
-          display: 'flex',
-          flex: 1,
-          flexDirection: 'column',
-          alignItems: 'start',
-          gap: 2
-        }}>
-          <Typography variant="h5" component="div">
-            Ready to level up your development experience with Okteto?
-            <ol>
-              <li>Install Okteto on your Kubernetes cluster</li>
-              <li>Add a context pointing to your cluster</li>
-            </ol>
-          </Typography>
-          <Box sx={{
+          gap: 6,
+        }}
+      >
+        <Box
+          sx={{
             display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
+            borderRight: '1px solid',
+            borderColor: 'white',
+            height: '100%',
             justifyContent: 'center',
-            gap: 1
-          }}>
-            <Button
-              variant="contained"
-              size="large"
-              sx={{ fontSize: '1.1rem' }}
-              onClick={handleTrial}
-            >
-              Install Okteto for Free
-            </Button>
-            <Button
+            pr: 6,
+          }}
+        >
+          <img
+            src={theme.palette.mode === 'dark' ? logoDark : logoLight}
+            width="240"
+          />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flex: 1,
+            flexDirection: 'column',
+            alignItems: 'start',
+            gap: 2,
+          }}
+        >
+          <Typography variant="h4" component="div" sx={{ maxWidth: '620px' }}>
+            Ready to level up your development experience with Okteto?
+          </Typography>
+
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+              gap: 1,
+              width: '100%',
+            }}
+          >
+            <Card
               variant="outlined"
-              size="large"
-              sx={{ fontSize: '1.1rem' }}
-              onClick={handleOpenDialog}
+              sx={{
+                width: '100%',
+                maxWidth: '500px',
+                background: 'transparent',
+                border: 0
+              }}
             >
-              Add your context
-            </Button>
+              <Box
+                sx={{
+                  m: 2,
+                  flexDirection: 'column',
+                  display: 'flex',
+                  gap: 1,
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: 1,
+                    alignItems: 'center',
+                  }}
+                >
+                  <Box
+                    sx={{
+                      backgroundColor:
+                        theme.palette.mode === 'dark'
+                          ? 'rgba(255, 255, 255, 0.12)'
+                          : 'rgba(0, 0, 0, 0.12)',
+                      borderRadius: '50%',
+                      width: '32px',
+                      height: '32px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      flexShrink: 0,
+                    }}
+                  >
+                    1
+                  </Box>
+                  <Typography variant="h5" component="div">
+                    Install Okteto on your Kubernetes cluster:
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    marginLeft: '40px'
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    size="large"
+                    sx={{ fontSize: '1.1rem' }}
+                    onClick={handleTrial}
+                  >
+                    Install Okteto for Free
+                  </Button>
+                </Box>
+              </Box>
+            </Card>
+
+            <Card
+              variant="outlined"
+              sx={{
+                width: '100%',
+                maxWidth: '500px',
+                background: 'transparent',
+                border: 0,
+              }}
+            >
+              <Box
+                sx={{
+                  m: 2,
+                  flexDirection: 'column',
+                  display: 'flex',
+                  gap: 1,
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: 1,
+                    alignItems: 'center',
+                  }}
+                >
+                  <Box
+                    sx={{
+                      backgroundColor:
+                        theme.palette.mode === 'dark'
+                          ? 'rgba(255, 255, 255, 0.12)'
+                          : 'rgba(0, 0, 0, 0.12)',
+                      borderRadius: '50%',
+                      width: '32px',
+                      height: '32px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      flexShrink: 0,
+                    }}
+                  >
+                    2
+                  </Box>
+                  <Typography variant="h5" component="div">
+                    Configure the context pointing to your cluster:
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    marginLeft: '40px'
+                  }}
+                >
+                  <Button
+                    variant="outlined"
+                    size="large"
+                    sx={{ fontSize: '1.1rem' }}
+                    onClick={handleOpenDialog}
+                  >
+                    Add your context
+                  </Button>
+                </Box>
+              </Box>
+            </Card>
           </Box>
         </Box>
       </Box>
-      <ContextDialog open={open} onClose={handleCloseDialog}/>
+      <ContextDialog open={open} onClose={handleCloseDialog} />
     </Box>
   );
 }
